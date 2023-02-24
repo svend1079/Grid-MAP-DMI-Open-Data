619a54ceff86e6c9e291c66c1c1ddba9f2f33ee8
svend1079 <svend.nnl@gmail.com>
2023-02-24 08:53:50 +0100

    Local changes and municID
    Allowing local changes without editing file name.
    Adding a 0 in front of all municId to match DMI Open Data

--- Create Grid Map.py
+++ Create Grid Map.py
@@ -3,17 +3,16 @@ import folium
 from folium import features
 import json
 from folium.plugins import Geocoder
-import requests
 
 m = folium.Map(tiles='https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}@2x.png', attr='CartoDB, Voyager',
                zoom_control=False, zoom_start=7.4,
                location=[56.05, 10.84])
 
-with open("C:/Users/B056860/Downloads/10x10km-Grid (3)/10x10km.geojson") as f:
+with open("10x10/10x10.geojson") as f:
     grid10x10 = json.load(f)
-with open("C:/Users/B056860/Downloads/20x20km-Grid (2)/20x20km.geojson") as f:
+with open("20x20/20x20.geojson") as f:
     grid20x20 = json.load(f)
-with open("C:/Users/B056860/Desktop/Map_test/Kommuner.geojson") as f:
+with open("Municipalities/Municipalities.geojson") as f:
     kommuner = json.load(f)
 
 style_function_20 = {'fillOpacity': '0.1', 'color': '#0066ff', 'weight': '1.5'}
@@ -24,16 +23,16 @@ g20 = folium.GeoJson(grid20x20, name='20x20km', style_function=lambda x: style_f
 g20.add_child(folium.features.GeoJsonPopup(fields=['cellId']))
 g10 = folium.GeoJson(grid10x10, name='10x10km', style_function=lambda x: style_function_10)
 g10.add_child(folium.features.GeoJsonPopup(fields=['cellId']))
-kom_feat = folium.GeoJson(kommuner, name='Kommuner', style_function=lambda x: style_function_kom)
-kom_feat.add_child(folium.features.GeoJsonPopup(fields=('Kommune', 'KommuneID')))
+kom_feat = folium.GeoJson(kommuner, name='Municipalities', style_function=lambda x: style_function_kom)
+kom_feat.add_child(folium.features.GeoJsonPopup(fields=['Name', 'MunicipalityId']))
 
 g20.add_to(m).show = False
 g10.add_to(m).show = False
 kom_feat.add_to(m).show = False
 Geocoder(add_marker=True).add_to(m)
 
-lay = folium.LayerControl(position='topleft', collapsed=False)
-lay.add_to(m)
+layControl = folium.LayerControl(position='topleft', collapsed=False, autoZIndex=True)
+layControl.add_to(m)
 
-m.save(r'C:/Users/B056860/Desktop/Map_test/DMI Grid and Municipalities.html')
+m.save('index.html')
 
